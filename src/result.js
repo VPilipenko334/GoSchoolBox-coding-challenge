5f3e033e180314bef9ac274ff7fff01fd933fd86
veronika <veronika.pilipenko@gmail.com>
2021-10-20 17:53:43 -0400

    input and result components exist

--- src/result.js
+++ src/result.js
@@ -5,6 +5,7 @@ const Result = () => {
         <div className="results-wrapper">
             <div className="result">
                 <h2>Result will go here</h2>
+                <h3>input will go here</h3>
             </div>
         </div>
     )
