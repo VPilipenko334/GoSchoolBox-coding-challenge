7c8f84663b103aaf53178f365226ddeb73a8815f
Veronika <veronika.pilipenko@gmail.com>
2021-10-22 16:39:23 -0400

    refactored a lot of code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Component } from "react";
-
 import Result from "./result";
 import Input from './input';
 import "./calculator.css";
@@ -24,44 +22,14 @@ class App extends React.Component {
     this.setState({ input: this.state.input + val });
   }
   
-  // const [input, setInput] = useState("")
-  // const [result, setResult] = useState("")
-
-  // { input === "" ? 0 : input };
-  
-
-  // const updateValues = (val) => {
-  //   setInput(input + val);
-
-  //   setResult("0");
-  // }
-
-  //not functional; needs additional logic 
-  // const addZero = (val) => {
-  //   if (this.state.input !== "") {
-  //     this.setInput(this.state.input + val)
-  //   }
-  // }
-
   allClear = () => {
     this.setState({ input: "" })
   }
 
-  // const goBack = () => {
-  //   setInput(input.slice(0, input.length -1)); 
-  // }
-  
-  // allDigits = () => {
-  //   let array = [];
-
-  //   for (let i = 0; i < 10; i++) {
-  //     array.push(
-  //       <button onClick={() => updateValues(i)} key={i}>{i}</button>
-  //     )
-  //   }
-
-  //   return array.reverse(); 
-  // }
+  goBack = () => {
+    this.setState({ input: this.state.input.slice(0, this.state.input.length) })
+  }
+ 
   render() {
   return (
       <div className="App">
@@ -69,40 +37,34 @@ class App extends React.Component {
             <div className="calculator-wrapper">
               <div className="calculator">
                 <div className="result-wrapper">
-                  {this.state.input}
-                  {this.state.result}
-                  {/* < Input />  */}
+                  < Input />
+                  < Result />
               </div>
             
             <div className="calculator-operators">
               <div className="numbers">
-                <button handleClick={this.updateValues}>9</button>
-                <button handleClick={this.updateValues}>8</button>
-                <button handleClick={this.updateValues}>7</button>
-                <button handleClick={this.updateValues}>6</button>
-                <button handleClick={this.updateValues}>5</button>
-                <button handleClick={this.updateValues}>4</button>
-                <button handleClick={this.updateValues}>3</button>
-                <button handleClick={this.updateValues}>2</button>
-                <button handleClick={this.updateValues}>1</button>
-                <button handleClick={this.updateValues}>0</button>
-                <button handleClick={this.updateValues}>.</button>
-                <button className="back-button">BK</button>
-
-                {/* {allDigits()} */}
-              {/* <button onClick={addZero}>0</button> */}
-              {/* <button onClick={() => updateValues('.')}>.</button> */}
-              {/* <button className="back-button">BK</button> */}
+                <button onClick={this.updateValues}>9</button>
+                <button onClick={this.updateValues}>8</button>
+                <button onClick={this.updateValues}>7</button>
+                <button onClick={this.updateValues}>6</button>
+                <button onClick={this.updateValues}>5</button>
+                <button onClick={this.updateValues}>4</button>
+                <button onClick={this.updateValues}>3</button>
+                <button onClick={this.updateValues}>2</button>
+                <button onClick={this.updateValues}>1</button>
+                <button onClick={this.updateValues}>0</button>
+                <button onClick={this.updateValues}>.</button>
+                <button className="back-button" onClick={this.goBack}>BK</button>
               </div>
 
               <div className="operators">
-                <button className="operator" handleClick={this.updateValues}>+</button>
-                <button className="operator" handleClick={this.updateValues}>-</button>
-                <button className="operator" handleClick={this.updateValues}>*</button>
-                <button className="operator" handleClick={this.updateValues}>/</button>
-                <button className="operator" handleClick={this.updateValues}>(</button>
-                <button className="operator" handleClick={this.updateValues}>)</button>
-                <button className="operator" handleClick={this.updateValues}>^</button>
+                <button className="operator" onClick={this.updateValues}>+</button>
+                <button className="operator" onClick={this.updateValues}>-</button>
+                <button className="operator" onClick={this.updateValues}>*</button>
+                <button className="operator" onClick={this.updateValues}>/</button>
+                <button className="operator" onClick={this.updateValues}>(</button>
+                <button className="operator" onClick={this.updateValues}>)</button>
+                <button className="operator" onClick={this.updateValues}>^</button>
                 <button className="operator">=</button>
             
               </div>
@@ -117,8 +79,8 @@ class App extends React.Component {
           </div>
         </div>
       </div>
-  );
-                }
+    );
+  }
 }
 
 export default App;
--- src/input.js
+++ src/input.js
@@ -1,38 +1,6 @@
 import React from "react";
 
 class Input extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            input: "",
-            num1: "",
-            num2: "",
-            operation: "",
-            result: ""
-        };
-    }
-
-    updateValues = (val) => {
-        this.setState({ input: this.state.input + val });
-    }
-
-    setNum1(event) {
-        const num1 = event.target.value ? parseInt(event.target.value) : "";
-        this.setState({ num1 });
-    }
-
-    setNum2(event) {
-        const num2 = event.target.value ? parseInt(event.target.value) : "";
-        this.setState({ num2})
-    }
-
-    add(e) {
-        e.preventDefault();
-        const result = this.state.num1 + this.state.num2;
-        this.setState({ result })
-    }
-
 
     render() {
         return (
