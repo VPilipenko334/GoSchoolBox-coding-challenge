9ddd1b4e91135b4fad30651b7693050be4d39781
Veronika <veronika.pilipenko@gmail.com>
2021-10-27 21:38:47 -0400

    final draft

--- src/App.js
+++ src/App.js
@@ -49,73 +49,12 @@ class App extends React.Component {
     this.setState({ number: this.state.input, input: firstNum + "^", operation: "exponent" })
   } 
 
-// base case === subtraction 
-//PEMDAS 
-
-// loop thrugh once and check for parenthesis
-// if '(' search from ')' --> evaluate parenthesis 
-// look for more opening parenthesis, if not, elseif statement for exponent 
-//no exponent --> keep going until subtraction 
-// check if there is another pair of parenthesis until no pair are left 
-// if no parenthesis are left in the whole thing; ;look for exponent 
-// keep evaluating until subtraction --> left to right 
-
-  // evaluate = () => {
-  //   const pemdas = [ '(', ')', '^', '*', '/', '+', '-']
-
-  //   for(let i = 0; i < pemdas.length; i++) {
-  //     if (this.state.input.includes('('))
-  //       for(let j = 0; j < ')'; j++); {
-  //         if (this.state.input.includes('('))
-  //         // if it includes another opening parenthesis; exit the loop and check for another one parenthesis
-  //         // keep checking for opening parenthesis until there are none left 
-  //         // check for exponents here as well, go through the whole thing 
-  //       } else {
-  //         if (this.state.input.includes('^'));
-  //           this.state.input ** this.state.input
-          // evaluate 
-          // continue checking if there are more exponents 
-          // if no exponents, look for multiplication 
-          // if no multiplication, look for division
-          // addition
-          //subtraction 
-  //       }
-  //     }
-  //   }
-
-  // }
-  // base case ==> for () would just be a number 
-  // remove parenthesis 
-
-
-  // equals = () => {
-  //   debugger 
-  //     if (this.state.operation === "add" ) {
-  //       this.setState({ result: parseInt(this.state.number) + parseInt(this.state.number) })
-  //     } else if (this.state.operation === "subtract") {
-  //       this.setState({ result: parseInt(this.state.input) - parseInt(this.state.number)})
-  //     } else if (this.operation === "multiply") {
-  //       this.setState({ result: parseInt(this.state.input) * parseInt(this.state.number)})
-  //     } else if (this.operation === "divide") {
-  //       this.setState({ result: parseInt(this.state.input) / parseInt(this.state.number )})
-  //     } else if (this.operation === "exponent") {
-  //       this.setState({ result: parseInt(this.state.input) ** parseInt(this.state.number )})
-  //     }
-  // }
-
-  
-  // This helper  looks for the first instance of a closing parenthesis,
-  // then backtracks to the corresponding opening parenthesis and finally
-  // evaluates the expression between the two parentheses
-  
-  
-  // This helper  recursively evaluates any expressions without parentheses,
-  // one operation at a time.
 
  evalEMDAS(expr) {
   // Split the expression on any simple operator using Regex
   // e.g. if expr === '1.0+3', then nums === ['1.0', '3']
   let nums = expr.split(/[\*\/\^\+\-]/);
+  //  let nums = expr.split(/[*/^+-]/);
 
   // If there are no operators remaining, return the only number
   if (nums.length === 1) {
@@ -173,20 +112,7 @@ class App extends React.Component {
     return this.evalEMDAS(this.merge(nums, operators));
   }
 
-  // Below is a quick (and failed) attempt to DRY up the previous code
-
-  // MDAS.forEach(operator => {
-  //     if (operators.includes(operator)) {
-  //         let oprIdx = operators.indexOf(operator);
-  //         let result = operate(parseFloat(nums[oprIdx]), parseFloat(nums[oprIdx + 1]), operator);
-  //         console.log("result: ", result);
-  //         nums.splice(oprIdx, 2, result.toString());
-  //         operators.splice(oprIdx, 1);
-  //         return evalEMDAS(merge(nums, operators));
-  //     }
-  // });
 }
-
   reduceParens(expr) {
     let closingIdx = expr.indexOf(")")
     let openingIdx = closingIdx - 1;
@@ -201,11 +127,11 @@ class App extends React.Component {
     // Return the original expression with one set of parentheses simplified
     return expr.slice(0, openingIdx) + simplified + expr.slice(closingIdx + 1);
   }
-  // const MDAS = ['*', '/', '+', '-'];
 
   reduceExpression(expr) {
 
     // First, we want to evaluate all sets of parentheses, one set at a time
+    // debugger 
     while (expr.includes(")")) {
       expr = this.reduceParens(expr);
     }
@@ -246,12 +172,6 @@ class App extends React.Component {
 //         case '-': return num1 - num2;
 //     }
 // }
-
-// console.log(reduceExpression("2^3+3*4-1")); // 19
-// console.log(reduceExpression("(((1+2)^2)^3+(5-2*2))/100")); // 7.3
-// console.log(reduceExpression("2.5/2.0")); // 1.25
-
-
 
   allClear = (e) => {
     e.preventDefault();
